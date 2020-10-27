8106f3dc764cc8d3a3c4a38b561301b437ae9089
Phil Brown <pdbrown.git@gmail.com>
2020-10-27 12:23:37 -0700

    Cleanup elpa

--- elpa/26.1/develop/dante-20200914.1856/dante-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dante" "20200914.1856" "Development mode for Haskell" '((dash "2.12.0") (emacs "25.1") (f "0.19.0") (flycheck "0.30") (company "0.9") (haskell-mode "13.14") (s "1.11.0") (lcr "1.0")) :commit "b3999b391122f23558665d651be72ddb0821604d" :keywords '("haskell" "tools") :authors '(("Jean-Philippe Bernardy" . "jeanphilippe.bernardy@gmail.com")) :maintainer '("Jean-Philippe Bernardy" . "jeanphilippe.bernardy@gmail.com") :url "https://github.com/jyp/dante")
--- elpa/26.1/develop/ron-mode-20200830.1554/ron-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ron-mode" "20200830.1554" "Rusty Object Notation mode" '((emacs "24.5.1")) :commit "c5e0454b9916d6b73adc15dab8abbb0b0a68ea22" :keywords '("languages") :authors '(("Daniel Hutzley" . "endergeryt@gmail.com")) :maintainer '("Daniel Hutzley" . "endergeryt@gmail.com") :url "https://chiselapp.com/user/Hutzdog/repository/ron-mode/home")
